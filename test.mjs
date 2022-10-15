a6e3ca5ccf218b7a9da406e2751336c4fc0822ec
TetroGem <benjamin11220364@gmail.com>
2022-10-15 10:16:43 -0500

    Removed test file

--- test.mjs
+++
@@ -1,9 +0,0 @@
-import fixFloat from "./index.mjs";
-
-console.log('Hello World');
-const float = 0.101 + 0.202;
-console.log(float);
-console.log(fixFloat(float));
-console.log(fixFloat(float, 3));
-console.log(fixFloat(float, 2));
-console.log(fixFloat(float, 1));
